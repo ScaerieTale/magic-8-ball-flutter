2da09687a03efdb5d23e0a5cad46bd2bac00e490
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-11-22 01:39:53 -0600

    Create stateful "logic" widget for the 8-ball to live inside.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,36 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-            backgroundColor: Colors.blue[500],
-            appBar: AppBar(
-              backgroundColor: Colors.blue[800],
-              title: Center(
-                child: Text(
-                  'Ask me anything!',
-                  style: TextStyle(color: Colors.white, fontSize: 36),
-                ),
+          backgroundColor: Colors.blue[500],
+          appBar: AppBar(
+            backgroundColor: Colors.blue[800],
+            title: Center(
+              child: Text(
+                'Ask me anything!',
+                style: TextStyle(color: Colors.white, fontSize: 36),
               ),
-            )),
+            ),
+          ),
+          body: Body8Ball(),
+        ),
       ),
     );
+
+class Body8Ball extends StatefulWidget {
+  const Body8Ball({Key key}) : super(key: key);
+
+  @override
+  State<Body8Ball> createState() => _Body8BallState();
+}
+
+class _Body8BallState extends State<Body8Ball> {
+  @override
+  int answer = 1;
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
