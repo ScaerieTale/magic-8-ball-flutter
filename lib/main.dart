7ae6904973784515b5e34a4ee38adcb82af4ae3d
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-11-22 01:35:19 -0600

    Set up Scaffold and app bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+            backgroundColor: Colors.blue[500],
+            appBar: AppBar(
+              backgroundColor: Colors.blue[800],
+              title: Center(
+                child: Text(
+                  'Ask me anything!',
+                  style: TextStyle(color: Colors.white, fontSize: 36),
+                ),
+              ),
+            )),
       ),
     );
